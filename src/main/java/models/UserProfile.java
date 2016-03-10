a949ce1ed4f8f4c1db68172ce896407e91bc83b9
Ruslan <vergilijus@ya.ru>
2016-03-10 15:44:33 +0300

    Добавил валидаторы полей UserProfile.

--- src/main/java/models/UserProfile.java
+++ src/main/java/models/UserProfile.java
@@ -3,10 +3,18 @@ package models;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class UserProfile {
     private static final AtomicLong ID_GENETATOR = new AtomicLong(0);
+    private static final int MIN_LOGIN_LENGTH = 3;
+    private static final int MIN_PASS_LENGTH = 5;
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*"
+            + "@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static Pattern emailPattern;
+
     @NotNull
     private String login;
     @NotNull
@@ -66,5 +74,26 @@ public class UserProfile {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public static boolean isLoginValid(String login) {
+        if (login == null) return false;
+        if (login.length() < MIN_LOGIN_LENGTH) return false;
+        return true;
+    }
+
+    public static boolean isPasswordValid(String password) {
+        if (password == null) return false;
+        if (password.length() < MIN_PASS_LENGTH) return false;
+        return true;
+    }
+
+    public static boolean isEmailValid(String email) {
+        if (email == null) return false;
+        if (emailPattern == null) {
+            emailPattern = Pattern.compile(EMAIL_PATTERN);
+        }
+        final Matcher emailMatcher = emailPattern.matcher(email);
+        return emailMatcher.matches();
     }
 }
