592c968f9a0a7b574e014311785af1531fa0d69a
Ruslan <vergilijus@ya.ru>
2016-03-11 01:08:36 +0300

    Перенес тесты в отдельную директорию.

--- src/main/test/models/UserProfileTest.java
+++ src/main/test/models/UserProfileTest.java
@@ -1,13 +1,11 @@
-package test;
+package models;
 
-import models.UserProfile;
 import org.junit.Test;
 
 import static models.UserProfile.isEmailValid;
 import static models.UserProfile.isLoginValid;
 import static models.UserProfile.isPasswordValid;
 import static org.junit.Assert.*;
-
 
 public class UserProfileTest {
 
@@ -27,19 +25,17 @@ public class UserProfileTest {
         final String[] invalidPasswords = {null, "", "1", "12", "123", "1234"};
         for (String password : validPasswords)
             assertTrue("Valid password validation fault", isPasswordValid(password));
-        for (String password: invalidPasswords)
+        for (String password : invalidPasswords)
             assertFalse("Invalid password validation fault", isPasswordValid(password));
     }
 
     @Test
     public void testIsEmailValid() throws Exception {
         final String[] validEmails = {"alex@mail.ru", "qwer.ty@gmail.com", "qw-rty@yandex.ru"};
-        final String[] invalidEmails = {null, "", "alexmail.ru", ".test@gmail.com", "test@ya.ru.", "test@", "@test.ru", "te..st@ya.ru"};
+        final String[] invalidEmails = {null, "", "@", "alexmail.ru", ".test@gmail.com", "test@ya.ru.", "test@", "@test.ru", "te..st@ya.ru"};
         for (String email : validEmails)
             assertTrue("Valid email validation fault", isEmailValid(email));
         for (String email : invalidEmails)
             assertFalse("Invalid email validation fault", isEmailValid(email));
-
-
     }
 }
