1398dd77ef1bb6824a5ea882f7c3e63a636e0863
Ruslan <vergilijus@ya.ru>
2016-03-14 13:21:18 +0300

    Заготовки тестов сессий.

--- src/main/test/rest/SessionTest.java
+++ src/main/test/rest/SessionTest.java
@@ -1,10 +1,34 @@
 package rest;
 
+import main.RestApplication;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by gantz on 13.03.16.
  */
-public class SessionTest {
+public class SessionTest extends JerseyTest{
+    @Override
+    protected Application configure() {
+        return new RestApplication();
+    }
 
+    @Test
+    public void testCheckAuth() throws Exception {
+
+    }
+
+    @Test
+    public void testUserLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testUserLogout() throws Exception {
+
+    }
 }
