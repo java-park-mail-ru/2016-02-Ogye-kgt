4b39b8eb40dfaf7b2e1795adb717706e94cf53d2
Ruslan <vergilijus@ya.ru>
2016-05-26 12:27:09 +0300

    Добавил класс для игрового поля.

---
+++ src/main/java/mechanics/GameField.java
@@ -0,0 +1,7 @@
+package mechanics;
+
+/**
+ * Created by gantz on 26.05.16.
+ */
+public class GameField {
+}
---
+++ src/main/java/mechanics/WebSocketService.java
@@ -0,0 +1,7 @@
+package mechanics;
+
+/**
+ * Created by gantz on 25.05.16.
+ */
+public class WebSocketService {
+}
